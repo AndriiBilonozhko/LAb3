85cbd07202b0431a033e29bc811cd34207d253d2
Андрей <chel999111@gmail.com>
2021-03-25 22:36:52 +0200

    last update

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,9 +1,17 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import static java.sql.DriverManager.getDriver;
 
 public class Tests extends SetupDriver {
 
@@ -85,25 +93,73 @@ public class Tests extends SetupDriver {
         WebElement getValues = driver.findElement(By.xpath("//*[text()='Get values']"));
         getValues.click();
         String display = driver.findElement(By.xpath("//*[@class='groupradiobutton']")).getText();
-        Assert.assertEquals(display, "Sex : Male\n" +
-                "Age group: 15 - 50");
+        Assert.assertEquals(display, "Sex : Male\n" + "Age group: 15 - 50");
 
 
     }
 
     @Test
-    public void fiveTest(){
+    public void fifthTest() {
         handlePopupWindow();
         WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
         inputForms.click();
         WebElement dropDownList = driver.findElement(By.xpath("//ul[@id='treemenu']//a[text()='Select Dropdown List']"));
         dropDownList.click();
-        WebElement dayList = driver.findElement(By.xpath("//*[@id='select-demo']"));
-        dayList.click();
-        WebElement friday = driver.findElement(By.xpath("//*[@id='select-demo']//*[text()='Friday']"));
-        friday.click();
-        String dayListText = dayList.getText();
-        Assert.assertEquals(dayListText, "Friday");
+        Select dayList = new Select(driver.findElement(By.xpath("//*[@id='select-demo']")));
+        dayList.selectByVisibleText("Friday");
+        String daySelection = driver.findElement(By.xpath("//*[@class='selected-value']")).getText();
+        Assert.assertEquals(daySelection, "Day selected :- Friday");
+
+    }
+
+    @Test
+    public void sixthTest() {
+        handlePopupWindow();
+        WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
+        inputForms.click();
+        WebElement dropDownList = driver.findElement(By.xpath("//ul[@id='treemenu']//a[text()='Select Dropdown List']"));
+        dropDownList.click();
+        Actions a = new Actions(driver);
+        a.keyDown(Keys.SHIFT);
+        a.keyDown(Keys.CONTROL);
+        a.moveToElement(driver.findElement(By.xpath("//*[@id='multi-select']//*[text()='New York']"))).click();
+        a.moveToElement(driver.findElement(By.xpath("//*[@id='multi-select']//*[text()='Florida']"))).click();
+        a.build().perform();
+        WebElement getAll = driver.findElement(By.xpath("//*[@id='printAll']"));
+        getAll.click();
+        String getallSelected = driver.findElement(By.xpath("//*[@class='getall-selected']")).getText();
+        Assert.assertEquals(getallSelected, "Options selected are : Florida,New York");
+    }
+
+    @Test
+
+    public void seventhTest() {
+        handlePopupWindow();
+        WebElement inputForms = driver.findElement(By.xpath("//a[text()='Input Forms']"));
+        inputForms.click();
+        WebElement ajaxForm = driver.findElement(By.xpath("//ul[@id='treemenu']//a[text()='Ajax Form Submit']"));
+        ajaxForm.click();
+        WebElement buttonSubmit = driver.findElement(By.xpath("//*[@id='btn-submit']"));
+        buttonSubmit.click();
+        WebElement fieldName = driver.findElement(By.xpath("//*[@id='title']"));
+
+
+    }
+
+    @Test
+
+    public void eighthTest() {
+        handlePopupWindow();
+        WebElement datePickers = driver.findElement(By.xpath("//*[text()='Date pickers']"));
+        datePickers.click();
+        WebElement bootsStrap = driver.findElement(By.xpath("//ul[@id='treemenu']//*[text()='Bootstrap Date Picker']"));
+        bootsStrap.click();
+        WebElement calendar = driver.findElement(By.xpath("//div[@class='input-group date']"));
+        calendar.click();
+        Calendar cal = Calendar.getInstance((TimeZone) driver.findElement(By.xpath("//*[@class='today day']")));
+        cal.add(Calendar.DATE, -1);
+
+
     }
 }
 
